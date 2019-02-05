06aa5a430adce5624a6e15c208e2b5524e2f2aba
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-02-06 01:10:44 +0300

    #CHPA-6: Написать кодовую базу на go

--- main.go
+++ main.go
@@ -16,9 +16,13 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("postgres", "postgres://postgres:12345@localhost/forna")
+		db, err := sql.Open("postgres", "postgres://forna_user:12345@localhost:5432/forna")
+		err1 := db.Ping()
 		if err != nil {
 			panic(err)
+		}
+		if err1 != nil {
+			panic(err1)
 		}
 		defer db.Close()
 		fmt.Fprint(w, "hello, 4na")
