e73588386192aa7c3f868c5d699abf45ef6df4d7
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-02-08 00:10:10 +0300

    #CHPA-9: Подключить систему миграций Added https://github.com/rubenv/sql-migrate

--- main.go
+++ main.go
@@ -12,24 +12,24 @@ import (
 func main() {
 
 	fmt.Println("added basic code and dep tools to control dependencies")
+	db, err := sql.Open("postgres", "postgres://forna_user:12345@localhost:5432/forna")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	pingErr := db.Ping()
+	if pingErr != nil {
+		panic(pingErr)
+	}
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("postgres", "postgres://forna_user:12345@localhost:5432/forna")
-		err1 := db.Ping()
-		if err != nil {
-			panic(err)
-		}
-		if err1 != nil {
-			panic(err1)
-		}
-		defer db.Close()
 		fmt.Fprint(w, "hello, 4na")
 	})
 	http.Handle("/", router)
-	err := http.ListenAndServe(":10045", nil)
-	if err != nil {
+	listenandserveErr := http.ListenAndServe(":10045", nil)
+	if listenandserveErr != nil {
 		panic(err)
 	}
 }
