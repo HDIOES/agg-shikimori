dbbf25005270f1e00ce0b57952ce0c6d83db1381
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-06-27 21:01:43 +0300

    fix sql

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"log"
-	"os"
 
 	"net/http"
 
@@ -35,15 +34,8 @@ type Configuration struct {
 }
 
 func main() {
-	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
 	log.Println("Application has been runned")
 	log.Println("Loading configuration...")
-
-	file, err := os.OpenFile("cpa.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalln("Failed to open log file:", err)
-	}
-	log.SetOutput(file)
 
 	configuration := Configuration{}
 	gonfigErr := gonfig.GetConf("configuration.json", &configuration)
--- migrations/20190317011701-create_table.sql
+++
@@ -1,9 +0,0 @@
-
--- +migrate Up
-CREATE TABLE ngramm (
-    ngramm_value varchar(3),
-    anime_id BIGINT REFERENCES anime(id),
-    PRIMARY KEY(ngramm_value, anime_id)
-);
-CREATE INDEX idx_ngramm_value ON ngramm(ngramm_value);
--- +migrate Down
