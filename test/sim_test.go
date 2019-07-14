f89b31e9d22d05bfafd12361257355b843496f0f
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-07-14 15:16:33 +0300

    #SU4NA-20: Написание тестов для проекта

--- test/sim_test.go
+++ test/sim_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"database/sql"
 	"io/ioutil"
-	"log"
 	"strconv"
 	"testing"
 	"time"
@@ -16,43 +15,43 @@ import (
 	gock "gopkg.in/h2non/gock.v1"
 )
 
-func preTest() (*sql.DB, *util.Configuration) {
+func preTest(t *testing.T) (*sql.DB, *util.Configuration) {
 	//create config
 	configuration := util.Configuration{}
 	gonfigErr := gonfig.GetConf("../configuration.json", &configuration)
 	if gonfigErr != nil {
-		panic(gonfigErr)
+		t.Fatal(gonfigErr)
 	}
 	//create db
 	db, err := sql.Open("postgres", configuration.DatabaseURL)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	db.SetMaxIdleConns(configuration.MaxIdleConnections)
 	db.SetMaxOpenConns(configuration.MaxOpenConnections)
 	timeout := strconv.Itoa(configuration.ConnectionTimeout) + "s"
 	timeoutDuration, durationErr := time.ParseDuration(timeout)
 	if durationErr != nil {
-		log.Println("Error parsing of timeout parameter")
-		panic(durationErr)
+		t.Fatal(durationErr)
 	} else {
 		db.SetConnMaxLifetime(timeoutDuration)
 	}
 	//clear db
-	clearDb(db)
+	clearDb(db, t)
 	return db, &configuration
 }
 
-func clearDb(db *sql.DB) (dbErr error) {
+func clearDb(db *sql.DB, t *testing.T) (dbErr error) {
 	tx, txErr := db.Begin()
 	if txErr != nil {
-		log.Println("Transaction start failed")
+		t.Log(txErr)
 		return txErr
 	}
 	defer func(tx *sql.Tx) {
 		if dbErr != nil {
+			t.Log(dbErr)
 			if err := tx.Rollback(); err != nil {
-				log.Println("Transaction cannot rollbaked")
+				t.Log(err)
 			}
 		}
 	}(tx)
@@ -73,27 +72,28 @@ func clearDb(db *sql.DB) (dbErr error) {
 		return dbErr
 	}
 	if txCommitErr := tx.Commit(); txCommitErr != nil {
-		log.Println("Transaction cannot be commited")
 		dbErr = txCommitErr
 		return dbErr
 	}
 	return nil
 }
 
-func postTest(db *sql.DB) {
-	db.Close()
+func postTest(db *sql.DB, t *testing.T) {
+	if err := db.Close(); err != nil {
+		t.Fatal(err)
+	}
 }
 
 //TestSimple function
-func TestSimple(t *testing.T) {
+func TestShikimoriJobSuccess(t *testing.T) {
 	defer gock.Off()
-	db, config := preTest()
-	defer postTest(db)
+	db, config := preTest(t)
+	defer postTest(db, t)
 	shikimoriJob := &integration.ShikimoriJob{Db: db, Config: *config}
 
 	animesData, err := ioutil.ReadFile("mock/shikimori_animes_success.json")
 	if err != nil {
-		//test error
+		t.Fatal(err)
 	}
 	gock.New(config.ShikimoriURL).
 		Get(config.ShikimoriAnimeSearchURL).
@@ -104,7 +104,7 @@ func TestSimple(t *testing.T) {
 
 	genresData, err := ioutil.ReadFile("mock/shikimori_genres_success.json")
 	if err != nil {
-		//test error
+		t.Fatal(err)
 	}
 	gock.New(config.ShikimoriURL).
 		Get(config.ShikimoriGenreURL).
@@ -113,7 +113,7 @@ func TestSimple(t *testing.T) {
 
 	studiosData, err := ioutil.ReadFile("mock/shikimori_studios_success.json")
 	if err != nil {
-		//test error
+		t.Fatal(err)
 	}
 	gock.New(config.ShikimoriURL).
 		Get(config.ShikimoriStudioURL).
@@ -122,7 +122,7 @@ func TestSimple(t *testing.T) {
 
 	oneAnimeData, err := ioutil.ReadFile("mock/one_anime_shikimori_success.json")
 	if err != nil {
-		//test error
+		t.Fatal(err)
 	}
 	gock.New(config.ShikimoriURL).
 		Get(config.ShikimoriAnimeSearchURL+"/").
