0a59306af921c923b7a693da57bf421eb95173f7
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2019-07-18 01:04:18 +0300

    #SU4NA-20: Написание тестов для проекта

--- test/shikimori_job_test.go
+++ test/shikimori_job_test.go
@@ -6,95 +6,23 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/HDIOES/cpa-backend/integration"
 	"github.com/HDIOES/cpa-backend/util"
 	_ "github.com/lib/pq"
+	migrate "github.com/rubenv/sql-migrate"
 	"github.com/tkanos/gonfig"
 
+	"github.com/ory/dockertest"
 	gock "gopkg.in/h2non/gock.v1"
 )
 
 var db *sql.DB
 var configuration *util.Configuration
-
-func TestMain(m *testing.M) {
-	db, configuration = prepareDbAndConfiguration(m)
-	code := m.Run()
-	os.Exit(code)
-}
-
-func prepareDbAndConfiguration(m *testing.M) (*sql.DB, *util.Configuration) {
-	//create config
-	configuration := util.Configuration{}
-	gonfigErr := gonfig.GetConf("../configuration.json", &configuration)
-	if gonfigErr != nil {
-		log.Fatal(gonfigErr)
-	}
-	//create db
-	db, err := sql.Open("postgres", configuration.DatabaseURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	db.SetMaxIdleConns(configuration.MaxIdleConnections)
-	db.SetMaxOpenConns(configuration.MaxOpenConnections)
-	timeout := strconv.Itoa(configuration.ConnectionTimeout) + "s"
-	timeoutDuration, durationErr := time.ParseDuration(timeout)
-	if durationErr != nil {
-		log.Fatal(durationErr)
-	} else {
-		db.SetConnMaxLifetime(timeoutDuration)
-	}
-	return db, &configuration
-}
-
-func clearDb(db *sql.DB, t *testing.T) (dbErr error) {
-	tx, txErr := db.Begin()
-	if txErr != nil {
-		t.Log(txErr)
-		return txErr
-	}
-	defer func(tx *sql.Tx) {
-		if dbErr != nil {
-			t.Log(dbErr)
-			if err := tx.Rollback(); err != nil {
-				t.Log(err)
-			}
-		}
-	}(tx)
-	if _, err := tx.Exec("DELETE FROM ANIME_STUDIO"); err != nil {
-		dbErr = err
-		return dbErr
-	}
-	if _, err := tx.Exec("DELETE FROM STUDIO"); err != nil {
-		dbErr = err
-		return dbErr
-	}
-	if _, err := tx.Exec("DELETE FROM ANIME_GENRE"); err != nil {
-		dbErr = err
-		return dbErr
-	}
-	if _, err := tx.Exec("DELETE FROM ANIME"); err != nil {
-		dbErr = err
-		return dbErr
-	}
-	if txCommitErr := tx.Commit(); txCommitErr != nil {
-		dbErr = txCommitErr
-		return dbErr
-	}
-	return nil
-}
-
-func postTest(db *sql.DB, t *testing.T) {
-	if err := db.Close(); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestShikimoriJobSuccess1(t *testing.T) {
-}
+var testContainer *dockertest.Resource
 
 //TestSimple function
 func TestShikimoriJobSuccess(t *testing.T) {
@@ -143,4 +71,119 @@ func TestShikimoriJobSuccess(t *testing.T) {
 		JSON(oneAnimeData)
 
 	shikimoriJob.Run()
+}
+
+func TestMain(m *testing.M) {
+	_, db, configuration, testContainer = prepareDbAndConfiguration(m)
+	code := m.Run()
+	testContainer.Close()
+	os.Exit(code)
+}
+
+func postTest(db *sql.DB, t *testing.T) {
+	if err := db.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+//prepareDbAndConfiguration function prepares data container for using in local testing
+//or uses already prepared data container in case of using docker-compose testing
+func prepareDbAndConfiguration(m *testing.M) (e error, db *sql.DB, configuration *util.Configuration, resource *dockertest.Resource) {
+	//create config
+	configuration = &util.Configuration{}
+	gonfigErr := gonfig.GetConf("configuration-test.json", configuration)
+	if gonfigErr != nil {
+		return gonfigErr, nil, nil, nil
+	}
+	if strings.Compare(os.Args[1], "docker") == 0 {
+		//use already prepared container
+
+	} else {
+		//start up new test data container
+		pool, err := dockertest.NewPool("")
+		if err != nil {
+			return err, nil, nil, nil
+		} else {
+			res, rErr := pool.Run("postgres", "11.4", []string{
+				"POSTGRES_USER=test_forna_user",
+				"POSTGRES_PASSWORD=12345",
+				"POSTGRES_DB=test_forna"})
+			time.Sleep(10 * time.Second)
+			resource = res
+			postgresURL := "postgres://test_forna_user:12345@localhost:" + res.GetPort("5432/tcp") + "/test_forna?sslmode=disable"
+			configuration.DatabaseURL = postgresURL
+			if rErr != nil {
+				return rErr, nil, nil, resource
+			}
+		}
+	}
+	//create db
+	db, err := sql.Open("postgres", configuration.DatabaseURL)
+	if err != nil {
+		return err, nil, nil, resource
+	}
+	db.SetMaxIdleConns(configuration.MaxIdleConnections)
+	db.SetMaxOpenConns(configuration.MaxOpenConnections)
+	timeout := strconv.Itoa(configuration.ConnectionTimeout) + "s"
+	timeoutDuration, durationErr := time.ParseDuration(timeout)
+	if durationErr != nil {
+		return durationErr, nil, nil, resource
+	} else {
+		db.SetConnMaxLifetime(timeoutDuration)
+	}
+	err = applyMigrations(db)
+	if err != nil {
+		return err, nil, nil, resource
+	}
+	return nil, db, configuration, resource
+}
+
+func clearDb(db *sql.DB, t *testing.T) (dbErr error) {
+	tx, txErr := db.Begin()
+	if txErr != nil {
+		t.Log(txErr)
+		return txErr
+	}
+	defer func(tx *sql.Tx) {
+		if dbErr != nil {
+			t.Log(dbErr)
+			if err := tx.Rollback(); err != nil {
+				t.Log(err)
+			}
+		}
+	}(tx)
+	if _, err := tx.Exec("DELETE FROM ANIME_STUDIO"); err != nil {
+		dbErr = err
+		return dbErr
+	}
+	if _, err := tx.Exec("DELETE FROM STUDIO"); err != nil {
+		dbErr = err
+		return dbErr
+	}
+	if _, err := tx.Exec("DELETE FROM ANIME_GENRE"); err != nil {
+		dbErr = err
+		return dbErr
+	}
+	if _, err := tx.Exec("DELETE FROM ANIME"); err != nil {
+		dbErr = err
+		return dbErr
+	}
+	if txCommitErr := tx.Commit(); txCommitErr != nil {
+		dbErr = txCommitErr
+		return dbErr
+	}
+	return nil
+}
+
+func applyMigrations(db *sql.DB) error {
+	migrations := &migrate.FileMigrationSource{
+		Dir: "../migrations",
+	}
+
+	if n, err := migrate.Exec(db, "postgres", migrations, migrate.Up); err == nil {
+		log.Printf("Applied %d migrations!\n", n)
+	} else {
+		return err
+	}
+	return nil
 }
